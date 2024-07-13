73341558f179dd7930bfb68b5899023fbef99c89
Chirag Sompura <chiragsompura@Chirags-MacBook-Air.local>
2024-07-13 19:35:08 +0530

    changes

---
+++ resources/views/frontend/app.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+    @include('scripts.header_scripts')
+</head>
+
+@if(Auth::id())
+<body class="hold-transition sidebar-mini layout-fixed">
+@else
+<body class="sidebar-collapse">
+@endif
+    <div class="wrapper">
+         @include('frontend.header')
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Dashboard</h1>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid"></div>
+                <!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        @include('frontend.footer')
+    </div>
+    <!-- ./wrapper -->
+    @include('scripts.footer_scripts')
+</body>
+
+</html>
